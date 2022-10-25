49cae763c33e646beece5bdbfb39ff5d7a0c54e6
Wadir Merotti <waldir_merotti@hotmail.com>
2022-10-25 14:36:07 -0300

    adicionando mais regras elegibilidade

---
+++ src/main/java/com/itau/elegibilidade/inss/application/regras/Regra6.java
@@ -0,0 +1,23 @@
+package com.itau.elegibilidade.inss.application.regras;
+
+import com.itau.elegibilidade.inss.application.regras.convenio.Convenios;
+import com.itau.elegibilidade.inss.application.regras.dominio.Dominios;
+import com.itau.elegibilidade.inss.application.regras.modalidade.Modalidades;
+import com.itau.elegibilidade.inss.entrypoint.RegraRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Regra6 implements Regra {
+
+    ParametrosRegra parametros = new ParametrosRegra(
+            Convenios.INSS.codigo,
+            Dominios.OP.codigo,
+            Modalidades.PORTABILIDADE.codigo);
+
+    @Override
+    public void executar(RegraRequest request) {
+
+        if (parametros.respeitaRequest(request))
+            System.out.println("Executando regra 6");
+    }
+}
